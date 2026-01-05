de5a2d2050412f2f6d869b99262caa4178036f2d
dhruti2206 <roropla2019@gmail.com>
2026-01-05 20:33:28 +0530

    day-1 code in c++

---
+++ day-1/prog-2.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+    int age;
+    string name;
+    cout<<"enter your name:"<<endl;
+    cin>>name;
+    cout<<"enter your age:"<<endl;
+    cin>>age;
+    cout<<"--Details--"<<endl;
+    cout<<"Name:"<<name<<endl;
+    cout<<"Age:"<<age<<endl;
+    return 0;
+}
