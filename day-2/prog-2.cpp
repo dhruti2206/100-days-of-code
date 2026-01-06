7d872e035f3ff6d6e10930d06ac60e3bd9b331a8
dhruti2206 <roropla2019@gmail.com>
2026-01-06 19:33:09 +0530

    day 2 of code in c++

---
+++ day-2/prog-2.cpp
@@ -0,0 +1,18 @@
+//largest of 3 numbers
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b,c;
+    cout<<"Enter 3 numbers: ";
+    cin>>a>>b>>c;
+    if(a>=b && a>=c){
+        cout<<a<<" is the largest number"<<endl;
+    }
+    else if(b>=a && b>=c){
+        cout<<b<<"is the largest number"<<endl;
+    }
+    else{
+        cout<<c<<"is the largest number"<<endl;
+    }
+    return 0;
+}
