220662ae68bd1328af2f25153d881cd569faef41
dhruti2206 <roropla2019@gmail.com>
2026-01-12 22:28:28 +0530

    day 8 of code in c++

---
+++ day-8/prog1.cpp
@@ -0,0 +1,20 @@
+//fibonicacci series 
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter number of terms: ";
+    cin >> n;
+
+    int a = 0, b = 1, next;
+
+    cout << "Fibonacci Series: ";
+    for (int i = 0; i < n; i++) {
+        cout << a << " ";
+        next = a + b;
+        a = b;
+        b = next;
+    }
+    return 0;
+}
