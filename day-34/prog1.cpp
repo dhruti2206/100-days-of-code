d9ce9f220e57943775496389dae5626bbfcc056a
dhruti2206 <roropla2019@gmail.com>
2026-02-08 20:56:11 +0530

    day 34 of coding in c++

---
+++ day-34/prog1.cpp
@@ -0,0 +1,26 @@
+//selection sort
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter the size of array: ";
+    cin>>n;
+    int arr[n];
+    cout<<"Enter the elements of array: ";
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    for(int i=0;i<n-1;i++){
+        int min_index=i;
+        for(int j=i+1;j<n;j++){
+            if(arr[j]<arr[min_index]){
+                min_index=j;
+            }
+        }
+        swap(arr[i],arr[min_index]);
+    }
+    cout<<"Sorted array: ";
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+}
