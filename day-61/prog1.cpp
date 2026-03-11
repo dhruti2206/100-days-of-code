91276537a7f98b7d2421b5356b23df09eb9fd909
dhruti2206 <roropla2019@gmail.com>
2026-03-11 19:46:06 +0530

    day 61 binary tree creation

---
+++ day-61/prog1.cpp
@@ -0,0 +1,41 @@
+//binary tree creation
+#include<iostream>
+using namespace std;
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+    Node(int val) : data(val), left(nullptr), right(nullptr) {}
+};
+void preOrder(Node* root) {
+    if (root == nullptr) return;
+    cout << root->data << " ";
+    preOrder(root->left);
+    preOrder(root->right);
+}
+void inOrder(Node* root) {
+    if (root == nullptr) return;
+    inOrder(root->left);
+    cout << root->data << " ";
+    inOrder(root->right);
+}
+void postOrder(Node* root) {
+    if (root == nullptr) return;
+    postOrder(root->left);
+    postOrder(root->right);
+    cout << root->data << " ";
+}
+int main() {
+    Node* root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+    cout << "Pre-order Traversal: ";
+    preOrder(root);
+    cout << "\nIn-order Traversal: ";
+    inOrder(root);
+    cout << "\nPost-order Traversal: ";
+    postOrder(root);
+    return 0;
+}
