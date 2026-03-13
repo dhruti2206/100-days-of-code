5edd0c8d5dae983d372357a03fb8acdbc1e761ef
dhruti2206 <roropla2019@gmail.com>
2026-03-13 19:37:15 +0530

    day 63 inorder traversal

---
+++ day-63/prog1.cpp
@@ -0,0 +1,25 @@
+//inorder traversal of a binary tree
+#include<iostream>
+using namespace std;
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+    Node(int val) : data(val), left(nullptr), right(nullptr) {}
+};
+void inOrder(Node* root) {
+    if (root == nullptr) return;
+    inOrder(root->left);
+    cout << root->data << " ";
+    inOrder(root->right);
+}
+int main() {
+    Node* root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+    cout << "In-order Traversal: ";
+    inOrder(root);
+    return 0;
+}
