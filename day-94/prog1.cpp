e2a0e2280101bfa5a273ce40c8eda532da75c9c3
dhruti2206 <roropla2019@gmail.com>
2026-04-29 19:23:19 +0530

    day 94

---
+++ day-94/prog1.cpp
@@ -0,0 +1,30 @@
+//Largest rectangle in histogram
+#include <iostream>
+#include <vector>
+#include <stack>
+using namespace std;
+
+int largestRectangleArea(vector<int>& heights) {
+    stack<int> s;
+    int maxArea = 0;
+    int n = heights.size();
+
+    for (int i = 0; i <= n; i++) {
+        while (!s.empty() && (i == n || heights[i] < heights[s.top()])) {
+            int h = heights[s.top()];
+            s.pop();
+            int w = s.empty() ? i : i - s.top() - 1;
+            maxArea = max(maxArea, h * w);
+        }
+        s.push(i);
+    }
+
+    return maxArea;
+}
+
+int main() {
+    vector<int> heights = {2, 1, 5, 6, 2, 3};
+    int area = largestRectangleArea(heights);
+    cout << "The largest rectangle area is: " << area << endl;
+    return 0;
+}
