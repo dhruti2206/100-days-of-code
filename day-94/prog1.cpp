16170f912eb7a01c2062ea3af432aaa535df352d
Dhruti Padasalgi <roropla2019@gmail.com>
2026-04-29 19:28:45 +0530

    day 94 largest Rectangle Area in histogram

--- day-94/prog1.cpp
+++ day-94/prog1.cpp
@@ -3,12 +3,10 @@
 #include <vector>
 #include <stack>
 using namespace std;
-
 int largestRectangleArea(vector<int>& heights) {
     stack<int> s;
     int maxArea = 0;
     int n = heights.size();
-
     for (int i = 0; i <= n; i++) {
         while (!s.empty() && (i == n || heights[i] < heights[s.top()])) {
             int h = heights[s.top()];
@@ -18,10 +16,8 @@ int largestRectangleArea(vector<int>& heights) {
         }
         s.push(i);
     }
-
     return maxArea;
 }
-
 int main() {
     vector<int> heights = {2, 1, 5, 6, 2, 3};
     int area = largestRectangleArea(heights);
